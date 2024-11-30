e088527b25b98d902176f485978f7c1a5590c9e2
Santosh <polojusanthosh9@gmail.com>
2024-11-30 10:52:24 +0530

    Add files via upload

---
+++ com.seledemo.selenium/src/test/java/com/seledemo/selenium/SeleDemoC.java
@@ -0,0 +1,10 @@
+package com.seledemo.selenium;
+import org.testng.annotations.Test;
+
+public class SeleDemoC {
+	@Test
+	public void Printselect() {
+		System.out.println("Hello Santosh");
+	}
+
+}
